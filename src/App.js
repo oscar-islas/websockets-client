b0f3a1ebf97497ff11487498f2832ce170cecd9a
Oscar Islas <oislasreyes@gmail.com>
2020-06-17 20:10:37 -0500

    Creando la estructura de ejemplo usando websockets

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="feed">
+        <div className="new-post">
+          <form>
+            <textarea placeholder="¿Qué estás pensando?" />
+            <input type="submit" value="Twittear" />
+          </form>          
+        </div>
+        <div className="tweet-container">
+          
+        </div>
+      </div>
+      <div className="right-sidebar">
+          <h4>Tendencias</h4>
+      </div>
     </div>
   );
 }
